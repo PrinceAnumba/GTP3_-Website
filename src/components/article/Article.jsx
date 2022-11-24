eef864c47324c4ad98164efb453ace8b86961c8c
Prince Anumba <princeanumba0@gmail.com>
2022-11-24 21:31:11 +0000

    footer done

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imageUrl, date, title}) => {
   return (
-    <div>article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className="gpt3__blog-container_article-image">
+        <img src={imageUrl} alt="blogimage" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div className="">
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -5,13 +5,11 @@ const CTA = () => {
   return (
     <div className='gpt3__cta section__margin' id='cta'>
       <div className="gpt3__cta-content">
-        <div className="">
-          <p>Request Early Access to Get Started</p>
-          <h3>Register today & start exploring the endless possiblities.</h3>
-        </div>
-        <div className="">
-          <button>Get Started</button>
-        </div>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__cta-button">
+        <button type='button'>Get Started</button>
       </div>
     </div>
   )
